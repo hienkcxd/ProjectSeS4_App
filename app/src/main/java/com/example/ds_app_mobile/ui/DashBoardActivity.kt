a564cc6c6febf16217d2d214351e2b1b0607b438
Hien <hien03kc@gmail.com>
2023-03-30 22:04:01 +0700

    update light/night mode

--- app/src/main/java/com/example/ds_app_mobile/ui/DashBoardActivity.kt
+++ app/src/main/java/com/example/ds_app_mobile/ui/DashBoardActivity.kt
@@ -3,10 +3,12 @@ package com.example.ds_app_mobile.ui
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 import com.example.ds_app_mobile.R
 import com.example.ds_app_mobile.databinding.ActivityDashBoardBinding
 import kotlin.math.log
@@ -23,11 +25,18 @@ class DashBoardActivity : AppCompatActivity() {
         sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE)
         nightMode = sharedPreferences.getBoolean("night", false)
         editor = sharedPreferences.edit()
-
         if(nightMode){
+            binding.btnDarkMode.isEnabled = false
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            binding.btnDarkMode.setTextColor(ContextCompat.getColor(this, R.color.active_button))
+            val newDrawable = ContextCompat.getDrawable(this, R.drawable.night_mode_active_50)
+            binding.btnDarkMode.setCompoundDrawablesRelativeWithIntrinsicBounds(null, newDrawable, null, null)
         }else{
+            binding.btnLightMode.isEnabled = false
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            binding.btnLightMode.setTextColor(ContextCompat.getColor(this, R.color.active_button))
+            val newDrawable = ContextCompat.getDrawable(this, R.drawable.light_mode_active_50)
+            binding.btnLightMode.setCompoundDrawablesRelativeWithIntrinsicBounds(null, newDrawable, null, null)
         }
 
         binding.btnDarkMode.setOnClickListener {
