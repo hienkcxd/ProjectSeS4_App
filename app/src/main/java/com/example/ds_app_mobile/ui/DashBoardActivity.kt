53f70ef228d639ab64d1a8938a5f761ff0cbb1c5
hienkcxd <70784911+hienkcxd@users.noreply.github.com>
2023-03-30 15:36:28 +0700

    update dashboard

--- app/src/main/java/com/example/ds_app_mobile/ui/DashBoardActivity.kt
+++ app/src/main/java/com/example/ds_app_mobile/ui/DashBoardActivity.kt
@@ -2,8 +2,11 @@ package com.example.ds_app_mobile.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.ds_app_mobile.R
 import com.example.ds_app_mobile.databinding.ActivityDashBoardBinding
+import kotlin.math.log
 
 class DashBoardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDashBoardBinding
@@ -11,5 +14,23 @@ class DashBoardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDashBoardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnDarkMode.setOnClickListener {
+            val nightMode = AppCompatDelegate.getDefaultNightMode()
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_NO) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                Log.i("MyTag", "darkmode")
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
+
+        binding.btnLightMode.setOnClickListener {
+            val nightMode = AppCompatDelegate.getDefaultNightMode()
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                Log.i("MyTag", "light mode")
+            }
+        }
     }
 }
